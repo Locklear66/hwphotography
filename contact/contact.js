716968e1da35fac10972f0e37a44f4282d42813b
Lance Wooley <samuel.wooley@live.com>
2023-01-14 16:50:07 -0600

    contact page now works!

--- contact/contact.js
+++ contact/contact.js
@@ -5,3 +5,17 @@ function openMenu() {
  function closeMenu() {
     document.body.classList.remove('menu--open')
  }
+
+ function contact(event){
+   event.preventDefault();
+   emailjs
+      .sendForm(
+         'service_uc6u3wm',
+         'template_bpavk3e',
+         event.target,
+         'rJeubM889V0KM5C2W' 
+
+      ).then(() => {
+         console.log('this worked')
+      })
+ }
